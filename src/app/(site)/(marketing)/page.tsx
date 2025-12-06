459478b373de882844699ffd40fd1a0eeea79813
Charlie Clark <clarkcharlie03@gmail.com>
2025-12-06 17:51:42 -0500

    updates

--- src/app/(site)/(marketing)/page.tsx
+++ src/app/(site)/(marketing)/page.tsx
@@ -2,13 +2,10 @@
 
 import { HeroSection } from "./_components/hero-section";
 
-import { CtaSection } from "./_components/cta-section";
-
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <CtaSection />
     </main>
   );
 }
